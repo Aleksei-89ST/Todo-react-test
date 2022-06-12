5014b246ca2f903d89ea98fc8319e1f70fad2ff6
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-06-12 23:11:05 +0400

    при нажатии происходит - задача выполнена + перечёркивает

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
-const Layout = ({children}) => {
-  return (
-    <div className='py-10 bg-gray-900 h-screen'>{children}</div>
-  )
-}
+const Layout = ({ children }) => {
+  return <div className="py-10 bg-gray-900 h-screen">{children}</div>;
+};
 
-export default Layout
+export default Layout;
--- src/components/screens/Home/Home.jsx
+++ src/components/screens/Home/Home.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import TodoItem from "./item/TodoItem";
 
-const Home = () => {
-  const todos = [
+
+  const data = [
     {
       id:'d4d34dj938jd',
       title: "Finish the essay collaboration",
@@ -19,11 +19,20 @@ const Home = () => {
       isCompleted: false,
     },
   ];
+  const Home = () => {
+    const [todos,setTodos] = useState(data)
+    const changeTodo = (id) => {
+      const copy = [...todos]
+      const current = copy.find(t => t.id === id)
+      current.isCompleted = !current.isCompleted
+      setTodos(copy)
+    }
+console.log(todos)
   return (
-    <div className="bg-slate-600 h-screen text-ellipsis">
-      <h1 className="text-2x1 font-bold text-center mb-10">Тестовое задание Frontend intern в Mindbox </h1>
-      {todos.map((todo) => (
-        <TodoItem key={todo.id} todo={todo}/>
+    <div className="text-white w-4/5 mx-auto">
+      <h1 className="text-2xl font-bold text-center mb-10">Тестовое задание Frontend intern в Mindbox </h1>
+      {data.map((todo) => (
+        <TodoItem key={todo.id} todo={todo} changeTodo={changeTodo} />
       ))}
     </div>
   );
